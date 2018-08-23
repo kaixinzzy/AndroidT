b63e163b8dad485af0853bc91b686fc6965d186e
zhangzhiya <zhangzhiya@mail.bcl.com.cn>
2018-08-23 18:34:12 +0800

    屏幕适配：头条方案，动态修改密度值。

--- app/src/main/java/com/zzy/def/base/APP.java
+++ app/src/main/java/com/zzy/def/base/APP.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.blankj.utilcode.util.Utils;
 import com.squareup.leakcanary.LeakCanary;
 import com.zzy.def.utils.CrashHandler;
+import com.zzy.def.utils.Density;
 
 /**
  * Created by BCLZzy on 2018/6/21.
@@ -18,6 +19,8 @@ public class APP extends Application {
 
         initLeakCanary();//初始化Activity内存泄漏检测工具LeakCanary
         CrashHandler.getInstance().init(this);//异常捕获
+        Density.setDensity(this);// 屏幕适配初始化
+        Density.setAppOrientation(true);// 屏幕适配使整个应用有效
         Utils.init(this);// 工具集合
     }
 
---
+++ app/src/main/java/com/zzy/def/utils/Density.java
@@ -0,0 +1,122 @@
+package com.zzy.def.utils;
+
+import android.app.Activity;
+import android.app.Application;
+import android.content.ComponentCallbacks;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.DisplayMetrics;
+
+/**
+ * 通过修改系统参数来适配android设备
+ */
+public class Density {
+
+    private static float appDensity;
+    private static float appScaledDensity;
+    private static DisplayMetrics appDisplayMetrics;
+    private static int barHeight;
+    // 当前主流分辨率1920*1080 和 1280*720，他们的比值都是1.777
+    // 参考：https://mta.qq.com/mta/data/device/resolution
+    private static float width = 360f;
+    private static float height = 640f;
+
+    /**
+     * 获取状态栏高度
+     *
+     * @param context
+     * @return
+     */
+    public static int getStatusBarHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen",
+                "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    /**
+     * 初始化Application时，必须调用此方法。
+     * @param application Application
+     */
+    public static void setDensity(@NonNull final Application application) {
+        //获取application的DisplayMetrics
+        appDisplayMetrics = application.getResources().getDisplayMetrics();
+        //获取状态栏高度
+        barHeight = getStatusBarHeight(application);
+
+        if (appDensity == 0) {
+            //初始化的时候赋值
+            appDensity = appDisplayMetrics.density;
+            appScaledDensity = appDisplayMetrics.scaledDensity;
+
+            //添加字体变化的监听
+            application.registerComponentCallbacks(new ComponentCallbacks() {
+                @Override
+                public void onConfigurationChanged(Configuration newConfig) {
+                    //字体改变后,将appScaledDensity重新赋值
+                    if (newConfig != null && newConfig.fontScale > 0) {
+                        appScaledDensity = application.getResources().getDisplayMetrics().scaledDensity;
+                    }
+                }
+
+                @Override
+                public void onLowMemory() {
+                }
+            });
+        }
+    }
+
+    /**
+     * 重新计算density值，并将其赋值给Activity,（可以在BaseActivity中调用）
+     * 需要在onCreate方法中的setContentView前面执行，才生效。
+     * @param activity Activity
+     * @param orientation 方向（true:width\false:height）
+     */
+    public static void setAppOrientation(@Nullable Activity activity, boolean orientation) {
+
+        float targetDensity;
+        if (orientation) {
+            targetDensity = appDisplayMetrics.widthPixels / width;
+        } else {
+            targetDensity = (appDisplayMetrics.heightPixels - barHeight) / height;
+        }
+
+        float targetScaledDensity = targetDensity * (appScaledDensity / appDensity);
+        int targetDensityDpi = (int) (160 * targetDensity);
+
+        // 将计算得到的值赋值给此Activity
+        DisplayMetrics activityDisplayMetrics = activity.getResources().getDisplayMetrics();
+        activityDisplayMetrics.density = targetDensity;
+        activityDisplayMetrics.scaledDensity = targetScaledDensity;
+        activityDisplayMetrics.densityDpi = targetDensityDpi;
+    }
+
+    /**
+     * 重新计算density值，并将其赋值给整个应用。
+     * 应用不调用外部UI，并且确定全部为竖屏or全部为横屏时，在Application中调用此方法，Activity中就不用调适配的方法了。
+     * @param orientation 方向（true:width\false:height）
+     */
+    public static void setAppOrientation(boolean orientation){
+
+        float targetDensity;
+        if (orientation) {
+            targetDensity = appDisplayMetrics.widthPixels / width;
+        } else {
+            targetDensity = (appDisplayMetrics.heightPixels - barHeight) / height;
+        }
+
+        float targetScaledDensity = targetDensity * (appScaledDensity / appDensity);
+        int targetDensityDpi = (int) (160 * targetDensity);
+
+        // 将计算得到的值赋值给整个应用
+        appDisplayMetrics.density = targetDensity;
+        appDisplayMetrics.scaledDensity = targetScaledDensity;
+        appDisplayMetrics.densityDpi = targetDensityDpi;
+    }
+
+}
