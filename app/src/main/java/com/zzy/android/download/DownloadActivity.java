020fdcf2da49d790597f223c250cb788c2fa846f
zhangzhiya <zhangzhiya@mail.bcl.com.cn>
2018-09-28 18:49:04 +0800

    下载...

--- app/src/main/java/com/zzy/android/download/DownloadActivity.java
+++ app/src/main/java/com/zzy/android/download/DownloadActivity.java
@@ -5,8 +5,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
@@ -15,12 +17,14 @@ import com.zzy.event.ac.R;
 import java.util.Vector;
 
 /**
- * 用系统DownloadManager下载http\https的文件
+ * 用系统DownloadManager下载http\https的文件，适合大文件的下载
  * 权限：
  *      <uses-permission android:name="android.permission.INTERNET" />
  *      <uses-permission android:name="android.permission.WRITE_EXTERNAL_STORAGE" />
+ *      <uses-permission android:name="android.permission.DOWNLOAD_WITHOUT_NOTIFICATION" />
  * 参考：
  *      https://www.jianshu.com/p/3e1467c05acf
+ *      https://blog.csdn.net/qq_19431333/article/details/52798105
  * 类似的可用库：https://github.com/lingochamp/okdownload
  */
 public class DownloadActivity extends AppCompatActivity {
@@ -33,8 +37,13 @@ public class DownloadActivity extends AppCompatActivity {
         @Override
         public void onReceive(Context context, Intent intent) {
             if (intent.getAction().equals(DownloadManager.ACTION_DOWNLOAD_COMPLETE)) {
-                DownloadManager.Query query = new DownloadManager.Query();
-//                query.setFilterById()
+                // 获取下载完成任务的taskId
+                long taskId = intent.getLongExtra(DownloadManager.EXTRA_DOWNLOAD_ID, -1);
+                Log.d(TAG, taskId + "任务已完成");
+            } else if (intent.getAction().equals(DownloadManager.ACTION_NOTIFICATION_CLICKED)) {
+                // 当用户点击通知栏下载的任务时，我们可以执行自己的逻辑
+            } else if (intent.getAction().equals(DownloadManager.ACTION_VIEW_DOWNLOADS)) {
+                //
             }
         }
     };
@@ -45,10 +54,15 @@ public class DownloadActivity extends AppCompatActivity {
         setContentView(R.layout.activity_download);
 
         // 注册广播，用来接收下载任务的状态
-        registerReceiver(mReceiver, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(DownloadManager.ACTION_DOWNLOAD_COMPLETE);// 下载完成广播
+        intentFilter.addAction(DownloadManager.ACTION_NOTIFICATION_CLICKED);// 用户点击下载通知栏
+        intentFilter.addAction(DownloadManager.ACTION_VIEW_DOWNLOADS);//
+        registerReceiver(mReceiver, new IntentFilter());
         mDownloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
         // 下载数据
-        mDownloadStatuses.add(new DownloadStatus("http://182.92.118.251:80/AutomatSys/download/version/2.08.T0.B6/apk_release.zip"));
+        mDownloadStatuses.add(new DownloadStatus("任务一", "一开始下载了", "http://182.92.118.251:80/AutomatSys/download/version/2.08.T0.B6/apk_release.zip"));
+//        mDownloadStatuses.add(new DownloadStatus("任务二", "二开始下载了", "http://apkc.mumayi.com/2015/03/06/92/927937/xingxiangyi_V3.1.3_mumayi_00169.apk"));
         // 开始下载
         for (int i = 0; i < mDownloadStatuses.size(); i++) {
             startDownload(mDownloadStatuses.get(i));
@@ -66,7 +80,7 @@ public class DownloadActivity extends AppCompatActivity {
      * @param downloadStatus
      */
     private void startDownload(DownloadStatus downloadStatus) {
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(downloadStatus.getFilePath()));
+        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(downloadStatus.getDownloadPath()));
         // 设置当网络连接类型为wifi or mobile时，允许下载。如果当前网络不符合条件，则下载会等待.
         request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_WIFI | DownloadManager
                 .Request.NETWORK_MOBILE);
@@ -80,9 +94,9 @@ public class DownloadActivity extends AppCompatActivity {
         // Request.VISIBILITY_HIDDEN：不显示该下载请求的Notification。如果要使用这个参数，需要在应用的清单文件中加上DOWNLOAD_WITHOUT_NOTIFICATION权限。
         request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE);
         // 通知栏显示的下载任务标题
-        request.setTitle("My Data Download");
+        request.setTitle(downloadStatus.getNotificationTitle());
         // 通知栏显示的下载任务描述
-        request.setDescription("Android Data download using DownloadManager.");
+        request.setDescription(downloadStatus.getNotificationDescription());
         // 是否允许MediaScanner扫描到这个文件。默认不允许。
         request.allowScanningByMediaScanner();
         // 是否允许我们下载的文件被系统的Downloads应用扫描管理，默认是允许的
@@ -90,24 +104,104 @@ public class DownloadActivity extends AppCompatActivity {
         // 是否允许计量式的网络连接执行下载操作，默认允许
         request.setAllowedOverMetered(true);
         // 设置下载路径及文件名
-        request.setDestinationInExternalPublicDir("/download/", "aaa".concat(".tmp"));
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, "aaa".concat(".tmp"));
 
         if (mDownloadManager != null) {
-            // 将下载任务添加到下载队列，并返回任务id，通过此id可以取消任务，重启任务等等
-            long taskId = mDownloadManager.enqueue(request);
+            // 把任务加入下载队列并返回taskId，以便后面用于查询下载信息。若网络不满足条件、Sdcard挂载中、超过最大并发数等异常会等待下载，正常则直接下载。
+            // 通过此taskId可以取消任务，重启任务等等
+            final long taskId = mDownloadManager.enqueue(request);
+            downloadStatus.setTaskId(taskId);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    while (true) {
+                        try {
+                            Thread.sleep(200);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+//                        queryStatus(taskId);
+                    }
+                }
+            }).start();
+
         } else {
             Log.d(TAG, "下载任务加载队列失败，原因：DownloadManager is null");
         }
     }
 
     /**
-     * 取消下载任务，同时会删除对应的文件
+     * 查询下载进度
+     * @param downloadStatus
+     */
+    private void queryStatus(DownloadStatus downloadStatus) {
+        DownloadManager.Query query = new DownloadManager.Query().setFilterById(downloadStatus.getTaskId());
+        Cursor cursor = null;
+        try {
+            cursor = mDownloadManager.query(query);
+            if (cursor != null && cursor.moveToFirst()) {
+                // taskId
+                String taskId = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_ID));
+                // 下载文件本地完整路径，包括文件名
+                String name = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_LOCAL_FILENAME));
+                downloadStatus.setLocalFilePath(name);
+                // 下载文件网络地址
+                String uri = cursor.getString(cursor.getColumnIndex(DownloadManager.COLUMN_URI));
+                // 截止目前已经下载的文件总大小
+                int currentSize = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+                // 下载文件的总大小
+                int count = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+                downloadStatus.setPercent(percent(currentSize, count));
+                // 下载状态
+                int status = cursor.getInt(cursor.getColumnIndexOrThrow(DownloadManager.COLUMN_STATUS));
+                switch (status) {
+                    case DownloadManager.STATUS_PAUSED:// 暂停
+                        downloadStatus.setStatus(DownloadStatus.Status.PAUSED);
+                        break;
+                    case DownloadManager.STATUS_PENDING:// 下载中
+                        downloadStatus.setStatus(DownloadStatus.Status.PENDING);
+                        break;
+                    case DownloadManager.STATUS_SUCCESSFUL:// 下载成功
+                        downloadStatus.setStatus(DownloadStatus.Status.SUCCESSFUL);
+                        break;
+                    case DownloadManager.STATUS_FAILED:// 下载失败
+                        downloadStatus.setStatus(DownloadStatus.Status.FAILED);
+                        break;
+                    default:// 未知状态
+                        downloadStatus.setStatus(DownloadStatus.Status.UNKNOWN);
+                        break;
+                }
+                Log.d(TAG, downloadStatus.getTaskId() + " " + downloadStatus.getPercent() + " name:" + name + " uri:" + uri);
+            }
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
+
+    /**
+     * 取消下载任务，同时会删除对应的文件和记录
      * @param taskId 任务id
      */
     private void cancelDownload(long... taskId) {
         if (mDownloadManager != null) {
             mDownloadManager.remove(taskId);
         }
+    }
+
+    /**
+     * 计算下载文件的百分比
+     * @param currentSize 当前下载的字节数
+     * @param count 文件的总字节数
+     * @return
+     */
+    public String percent(int currentSize, int count) {
+        if (currentSize <= 0 || count < 0) {
+            return "";
+        }
+        int part = count/100;
+        return currentSize/part + "%";
     }
 
 
--- app/src/main/java/com/zzy/android/download/DownloadStatus.java
+++ app/src/main/java/com/zzy/android/download/DownloadStatus.java
@@ -4,17 +4,44 @@ public class DownloadStatus {
 
     private String NotificationTitle;// 通知栏标题
     private String NotificationDescription;// 通知栏描述
-    private String filePath;// 文件下载路径
-    private long taskId;// 任务id
 
-    public DownloadStatus(String filePath) {
-        this.filePath = filePath;
+    private long taskId;// 任务id
+    private String downloadPath;// 下载文件网络地址
+    private String localFilePath;// 下载后的本地文件完整路径（带文件名）
+    private Status status = Status.UNKNOWN;// 下载状态
+    private String percent;// 下载进度百分比
+
+    enum Status {
+        PAUSED("暂停"),
+        PENDING("下载中"),
+        SUCCESSFUL("下载成功"),
+        FAILED("下载失败"),
+        UNKNOWN("未知状态");
+
+        private String desc;//中文描述
+
+        Status(String desc) {
+            this.desc = desc;
+        }
+
+        public String getDesc() {
+            return desc;
+        }
+
+        @Override
+        public String toString() {
+            return desc;
+        }
     }
 
-    public DownloadStatus(String notificationTitle, String notificationDescription, String filePath) {
+    public DownloadStatus(String downloadPath) {
+        this.downloadPath = downloadPath;
+    }
+
+    public DownloadStatus(String notificationTitle, String notificationDescription, String downloadPath) {
         NotificationTitle = notificationTitle;
         NotificationDescription = notificationDescription;
-        this.filePath = filePath;
+        this.downloadPath = downloadPath;
     }
 
     public String getNotificationTitle() {
@@ -33,12 +60,20 @@ public class DownloadStatus {
         NotificationDescription = notificationDescription;
     }
 
-    public String getFilePath() {
-        return filePath;
+    public String getDownloadPath() {
+        return downloadPath;
     }
 
-    public void setFilePath(String filePath) {
-        this.filePath = filePath;
+    public void setDownloadPath(String downloadPath) {
+        this.downloadPath = downloadPath;
+    }
+
+    public String getLocalFilePath() {
+        return localFilePath;
+    }
+
+    public void setLocalFilePath(String localFilePath) {
+        this.localFilePath = localFilePath;
     }
 
     public long getTaskId() {
@@ -47,5 +82,21 @@ public class DownloadStatus {
 
     public void setTaskId(long taskId) {
         this.taskId = taskId;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public String getPercent() {
+        return percent;
+    }
+
+    public void setPercent(String percent) {
+        this.percent = percent;
     }
 }
